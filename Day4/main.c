689d3b516ca685c5aee5c3ec4112dc9548270152
WQH98 <2361125846@qq.com>
2023-07-03 09:45:33 +0800

    2023年7月3日提交 day4上午完成

--- Day4/main.c
+++ Day4/main.c
@@ -11,6 +11,7 @@ void  one_dimensional_array(void) {
     int i = 0;
     // sizeof(a) = 4 * 8 = 32   数组类型：由元素个数 和 元素类型组成   相当于8个int类型
     // sizeof(a[0])  首元素大小  每个元素4个字节
+    // 局限性：如果当时定义数组时[]内写的是100 则求出的n就为100
     n = sizeof(a) / sizeof(a[0]);    // n = 8;
 
     for(i = 0; i < n; i++) {
@@ -30,12 +31,22 @@ void  one_dimensional_array(void) {
     // 数组类型由元素个数和元素类型决定
     // 通过typedef定义一个数组类型
     // 有typedef是类型 没有就是变量
-    typedef int A[8];  // 代表是一个数组类型
+    typedef int A[8];  // 代表是一个数组类型 他是一个类型 不是一个变量
     // typedef int (A)[8]; // 与上面等价
 
     A b;   // 等价于int b[8] 删掉了typedef b被替换到了A的位置
 
-    
+    for(i = 0; i < 8; i++) {
+        b[i] = 2 * i + 1;
+    }
+    for(i = 0; i < 8; i++) {
+        // printf("%d ", b[i]);    // 与下面写法等价
+        printf("%d ", *(b + i));
+    }
+    printf("\r\n");
+
+    printf("b: %d, b+1: %d\r\n", b, b+1);
+    printf("&b: %d, &b+1: %d\r\n", &b, &b+1);
 
 }
 
